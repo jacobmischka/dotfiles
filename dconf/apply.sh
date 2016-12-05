36bb995d7df31681e1217d334aa31f5230726725
Jacob Mischka <jacobmischka@gmail.com>
2016-12-05 08:16:50 -0600

    Fix apply again, relative paths are weird in bash

--- dconf/apply.sh
+++ dconf/apply.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-dconf load /org/gnome/shell/keybindings/ < org.gnome.shell.keybindings
-dconf load /org/gnome/desktop/keybindings/ < org.gnome.desktop.keybindings
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ < org.gnome.settings-daemon.plugins.media-keys
+dconf load /org/gnome/shell/keybindings/ < ~/.dotfiles/dconf/org.gnome.shell.keybindings
+dconf load /org/gnome/desktop/keybindings/ < ~/.dotfiles/dconf/org.gnome.desktop.keybindings
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < ~/.dotfiles/dconf/org.gnome.settings-daemon.plugins.media-keys
