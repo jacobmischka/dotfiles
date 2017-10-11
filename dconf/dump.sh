61fdde97223d302cb3577312b301b67d7c23a49d
Jacob Mischka <jacob@mischka.me>
2017-10-11 08:06:17 -0500

    Add wm keybindings

--- dconf/dump.sh
+++ dconf/dump.sh
@@ -2,5 +2,6 @@
 
 dconf dump /org/gnome/shell/keybindings/ > ~/.dotfiles/dconf/org.gnome.shell.keybindings
 dconf dump /org/gnome/desktop/keybindings/ > ~/.dotfiles/dconf/org.gnome.desktop.keybindings
+dconf dump /org/gnome/desktop/wm/keybindings/ > ~/.dotfiles/dconf/org.gnome.desktop.wm.keybindings
 dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > ~/.dotfiles/dconf/org.gnome.settings-daemon.plugins.media-keys
 dconf dump /org/gnome/terminal/legacy/ > ~/.dotfiles/dconf/org.gnome.terminal.legacy
